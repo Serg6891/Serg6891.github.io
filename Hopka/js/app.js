203efa7b32963eb9e645cadf71dffa76d3cbc7f9
Serg6891 <61929399+Serg6891@users.noreply.github.com>
2020-03-08 12:39:05 +0300

    Add files via upload

---
+++ Hopka/js/app.js
@@ -0,0 +1,18 @@
+alert('Для моих самых дорогих и близких. Я Вас очень люблю и ценю!!');
+//хрень как надо смотреть src
+/*
+не забудь закрыть
+*/
+// ню ню
+// 10; literal
+// слово то же literal
+/*
+let ageA, ageB, ageC ; //var
+ageA = 8;
+ageB = 2;
+ageC = 6;
+console.log( "ageA=", ageA);
+console.log( "ageB=", ageB);
+ageB = ageC;
+console.log(ageA+ageB, console);
+*/
